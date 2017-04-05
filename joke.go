85f96c55eeadeffde131fa86205eaa178b0efa0b
Muninn <hyacinthus@gmail.com>
2017-04-06 00:15:39 +0800

    add post and put to joke

--- joke.go
+++ joke.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo"
 	mgo "gopkg.in/mgo.v2"
@@ -15,8 +16,8 @@ type joke struct {
 	Answer  string        `bson:"answer,omitempty" json:"answer,omitempty"`
 	VIA     string        `bson:"via,omitempty" json:"via,omitempty"`
 	URL     string        `bson:"url,omitempty" json:"url,omitempty"`
-	Created string        `bson:"_created,omitempty" json:"created,omitempty"`
-	Updated string        `bson:"_updated,omitempty" json:"updated,omitempty"`
+	Created time.Time     `bson:"_created,omitempty" json:"created,omitempty"`
+	Updated time.Time     `bson:"_updated,omitempty" json:"updated,omitempty"`
 	ETag    string        `bson:"_etag,omitempty" json:"-"`
 }
 
@@ -38,7 +39,7 @@ func getJoke(c echo.Context) error {
 }
 
 func getRandomJokes(c echo.Context) error {
-	// init size
+	// init page size
 	var size int
 	var err error
 	reqSize := c.QueryParam("size")
@@ -59,4 +60,64 @@ func getRandomJokes(c echo.Context) error {
 		return err
 	}
 	return c.JSON(http.StatusOK, jokes)
+}
+
+func createJoke(c echo.Context) error {
+	j := &joke{
+		ID:      bson.NewObjectId(),
+		Created: bson.Now(),
+	}
+	if err := c.Bind(j); err != nil {
+		return newHTTPError(http.StatusBadRequest, "InvalidRequestData", err.Error())
+	}
+	// save to db
+	col := db.C("joke")
+	err := col.Insert(j)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, j)
+}
+
+func updateJoke(c echo.Context) error {
+	// get id
+	id := c.Param("id")
+	if !bson.IsObjectIdHex(id) {
+		return newHTTPError(http.StatusBadRequest, "InvalidID", "invalid request joke id")
+	}
+	// get data
+	var j = new(joke)
+	if err := c.Bind(j); err != nil {
+		return newHTTPError(http.StatusBadRequest, "InvalidRequestData", err.Error())
+	}
+	var data = make(bson.M)
+	data["_updated"] = bson.Now()
+	if j.Content != "" {
+		data["content"] = j.Content
+	}
+	if j.Answer != "" {
+		data["answer"] = j.Answer
+	}
+	if j.VIA != "" {
+		data["via"] = j.VIA
+	}
+	if j.URL != "" {
+		data["url"] = j.URL
+	}
+	// update db
+	col := db.C("joke")
+	err := col.UpdateId(id, data)
+	if err == mgo.ErrNotFound {
+		return newHTTPError(http.StatusBadRequest, "InvalidID", err.Error())
+	}
+	if err != nil {
+		return err
+	}
+	// return new joke
+	var newj = new(joke)
+	err = col.FindId(bson.ObjectIdHex(id)).One(&newj)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, newj)
 }
